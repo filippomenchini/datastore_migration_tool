891fb37cedc8873c1eda9795b5b4f31966663364
Filippo Menchini <filippo.menchini99@gmail.com>
2022-08-25 10:58:10 +0200

    Added simple parallel scan

--- libs/data/scanTable.js
+++ libs/data/scanTable.js
@@ -1,40 +1,15 @@
-import { ScanCommand } from "@aws-sdk/lib-dynamodb";
-import { ddbDocClient } from "./dynamodbDocumentClient.js";
+import { parallelScan } from "@shelf/dynamodb-parallel-scan";
 
 async function scanTable(tableName) {
-    let records = [];
-    let startKey = null;
-    console.log("\nTable scan started...\n");
-    do {
-        // Getting data from the table.
-        const result = await scan(tableName, startKey);
-        console.log("Scanned records:", result.Items.length);
-        // Pushing records to the result.
-        records.push(result.Items);
-        // Updating key for next scan.
-        startKey = result.LastEvaluatedKey;
-    } while(startKey != null);
-    console.log("\nTable scan completed!\n");
-
-    return records.flat();
-}
-
-async function scan(tableName, startKey) {
     const scanParams = {
         TableName: tableName
     };
 
-    if (startKey != null) {
-        scanParams.ExclusiveStartKey = startKey
-    }
-    
-    try {
-        const data = await ddbDocClient.send(new ScanCommand(scanParams));
-        return data;
-    } catch (error) {
-        console.log("Unable to retreive data.");
-        return null;
-    }
+    console.log("\nScanning table:", tableName);
+    console.log("Please wait...\n");
+
+    const items = await parallelScan(scanParams, { concurrency: 1000 });
+    return items;
 }
 
 export { scanTable };
